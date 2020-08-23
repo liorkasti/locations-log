71120f5713224c67616e4e497d5a1072e92b2497
liorkasti <liorkasti@gmail.com>
2020-08-23 11:12:31 +0300

    Modify Action menu designs

--- src/components/ActionMenu.js
+++ src/components/ActionMenu.js
@@ -6,30 +6,31 @@ import {
     TouchableOpacity,
     Dimensions
 } from "react-native";
-import Icon from "react-native-vector-icons/AntDesign";
+// import Icon from "react-native-vector-icons/Feather";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 import FontAwesomeIcon from "react-native-vector-icons/MaterialIcons";
+// import Feather from "react-native-vector-icons/MaterialIcons";
 
 
 const ActionMenu = (props) => {
     return (
         <View style={styles.container}>
             <View style={styles.group}>
-                <TouchableOpacity
+                {/* <TouchableOpacity
                     onPress={() => { props.onActionMenu("onOpenLocation") }}
                     tag="Open Location"
                     style={styles.actionButton}
                 >
                     <Text style={styles.textMenuItem}>{props.tag || 'Open Location'}</Text>
                     <FontAwesomeIcon name="add-location" style={styles.icon} />
-
-                </TouchableOpacity>
+                </TouchableOpacity> */}
                 <TouchableOpacity
                     onPress={() => { props.onActionMenu("addLocation") }}
                     tag="Add Location"
                     style={styles.actionButton}
                 >
                     <Text style={styles.textMenuItem}>{props.tag || 'Add Location'}</Text>
-                    <FontAwesomeIcon name="add-location" style={styles.icon} />
+                <FontAwesomeIcon name="add-location" style={styles.icon} />
                 </TouchableOpacity>
                 <TouchableOpacity
                     onPress={() => { props.onActionMenu("editCategory") }}
@@ -37,7 +38,7 @@ const ActionMenu = (props) => {
                     style={styles.actionButton}
                 >
                     <Text style={styles.textMenuItem}>{props.tag || 'Edit Location'}</Text>
-                    <FontAwesomeIcon name="add-location" style={styles.icon} />
+                    <FontAwesomeIcon name="edit-location" style={styles.icon} />
 
                 </TouchableOpacity>
 
@@ -46,8 +47,8 @@ const ActionMenu = (props) => {
                     tag="Delete Location"
                     style={styles.actionButton}
                 >
-                    <Text style={styles.textMenuItem}>{props.tag || 'Add Location'}</Text>
-                    <FontAwesomeIcon name="add-location" style={styles.icon} />
+                    <Text style={styles.textMenuItem}>{props.tag || 'Delete Location'}</Text>
+                    <Icon name="map-marker-off-outline" style={styles.icon} />
                 </TouchableOpacity>
             </View>
         </View>
@@ -60,35 +61,43 @@ const styles = StyleSheet.create({
     container: {
         position: "absolute",
         flex: 1,
-        backgroundColor: "rgba(0,88,155,1)",
+        backgroundColor: "rgba(0,88,155,1.0)",
         width: 'auto',
         height: 'auto',
         top: 62,
         zIndex: 5000,
-        left: windowWidth - 180,
+        left: windowWidth - 170,
         // left: 0,
         // alignItems: 'center',
-        // right: 0,
+        // right: -2000,
     },
     group: {
         alignItems: "flex-end",
         zIndex: 20,
         justifyContent: "center",
-        margin: 20,
+        width: 170,    
+        height: 140        
     },
     textMenuItem: {
-        textAlign: "center",
+        textAlign: 'right', 
+        alignSelf: 'stretch',
         color: 'white',
         fontSize: 16,
+        borderBottomWidth:0.7,  
+        borderColor: 'white',
+        width: 200,
     },
     actionButton: {
         padding: 8,
         flexDirection: "row",
+        // paddingHorizontal: 10
     },
     icon: {
         color: 'white',
         fontSize: 20,
-        paddingHorizontal: 10
+        paddingHorizontal: 5,
+        textAlign: 'right', 
+        alignSelf: 'stretch',
       },});
 
 export default ActionMenu;
